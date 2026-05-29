45d47ef91284e05d002c4435b9f1b53dd3baace5
Menuka Dewsith <menukadewsith@Dewsiths-MacBook-Air.local>
2026-05-29 06:39:49 +0530

    Simplify routes to debug 500 error

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,19 @@
 <?php
 
+use Illuminate\Support\Facades\Route;
+
 Route::get('/', function () {
-    return 'PHP is working!';
+    return 'Welcome to Study Share! This is a simple test.';
 });
 
 Route::get('/test', function () {
     return 'Test route works!';
+});
+
+Route::get('/error-test', function () {
+    try {
+        return 'Inertia test: ' . Inertia::getVersion();
+    } catch (\Exception $e) {
+        return 'Error: ' . $e->getMessage();
+    }
 });
